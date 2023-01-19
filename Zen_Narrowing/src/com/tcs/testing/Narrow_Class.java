5318035e9b1586718ab529730948a8c9c8a13870
Moulirajsmr <mouli.rock07@gmail.com>
2023-01-19 22:02:13 +0530

    project one

---
+++ Zen_Narrowing/src/com/tcs/testing/Narrow_Class.java
@@ -0,0 +1,20 @@
+package com.tcs.testing;
+
+public class Narrow_Class {
+	
+	public static void main(String[] args) {
+		
+		int s = 89;
+		long h = s;
+		
+		System.out.println("the value of long =" + h);
+		
+		int j =  (int) h ;
+		
+		System.out.println("the value of int =" + j);
+	}
+	
+	
+	
+
+}
